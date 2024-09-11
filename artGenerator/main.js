107a2f807f9df2cd9c2e4ac9119f641b5e845644
Rafael Matias <rafamatias102@gmail.com>
2024-09-11 17:43:06 +0100

    Implemented input control feature for basic output manipulation. Improved UI.

--- artGenerator/main.js
+++ artGenerator/main.js
@@ -1,3 +1,102 @@
+
+// CANVAS CONSTANTS
+const radioLandscapeShape = document.querySelector('#landscape');
+const radioPortraitShape = document.querySelector('#portrait');
+const radioSquareShape = document.querySelector('#square');
+const radioCircleShape = document.querySelector('#circle');
+const canvasBackgroundColor = document.querySelector('#background-color');
+const canvasFrame = document.querySelector('#frame');
+
+// CANVAS INPUT-CONTROLS EVENT LISTNERS
+
+radioLandscapeShape.addEventListener('input', () => {
+    canvasFrame.style.width = '100px solid rgba(255, 255, 255, 255)'
+})
+
+canvasBackgroundColor.addEventListener('input', () => {
+    document.body.style.backgroundColor = canvasBackgroundColor.value
+})
+
+// SHAPE CONSTANTS
+
+    // General Inputs
+    const inputShapeAmount = document.querySelector('#shapes-amount');
+    const inputMaxWidth = document.querySelector('#max-width');
+    const inputMaxHeight = document.querySelector('#max-height');
+
+    // General Displays
+    const amountValue = document.querySelector('#amount-value');
+    const maxWidthValue = document.querySelector('#max-width-value');
+    const maxHeightValue = document.querySelector('#max-height-value');
+
+    // Color Inputs
+    const inputRed = document.querySelector('#red');
+    const inputGreen = document.querySelector('#green');
+    const inputBlue = document.querySelector('#blue');
+    const inputAlpha = document.querySelector('#alpha');
+    
+    // Color Displays
+    const redValue = document.querySelector('#r-value');
+    const greenValue = document.querySelector('#g-value');
+    const blueValue = document.querySelector('#b-value');
+    const alphaValue = document.querySelector('#a-value');
+
+    // Position Inputs
+    const inputX = document.querySelector('#x');
+    const inputY = document.querySelector('#y');
+    const inputZ = document.querySelector('#z');
+
+    // Position Displays
+    const xValue = document.querySelector('#x-value');
+    const yValue = document.querySelector('#y-value');
+    const zValue = document.querySelector('#z-value');
+
+
+
+// SHAPE INPUT-CONTROLS EVENT LISTNERS
+
+    // Display current COLOR value on dial range input
+    inputRed.addEventListener('input', () => {
+        redValue.textContent = inputRed.value;
+    });
+    inputGreen.addEventListener('input', () => {
+        greenValue.textContent = inputGreen.value;
+    });
+    inputBlue.addEventListener('input', () => {
+        blueValue.textContent = inputBlue.value;
+    });
+    inputAlpha.addEventListener('input', () => {
+        alphaValue.textContent = inputAlpha.value;
+    });
+
+    // Display current POSITION value on dial range input
+
+    inputX.addEventListener('input', () => {
+        xValue.textContent = inputX.value;
+    });
+    inputY.addEventListener('input', () => {
+        yValue.textContent = inputY.value;
+    });
+    inputZ.addEventListener('input', () => {
+        zValue.textContent = inputZ.value;
+    });
+
+    // Display current AMOUNT, MAX-WIDTH and MAX-HEIGHT value on dial range input
+
+    inputShapeAmount.addEventListener('input', () => {
+        amountValue.textContent = inputShapeAmount.value;
+    })
+    inputMaxWidth.addEventListener('input', () => {
+        maxWidthValue.textContent = inputMaxWidth.value;
+    })
+    inputMaxHeight.addEventListener('input', () => {
+        maxHeightValue.textContent = inputMaxHeight.value;
+    })
+
+
+
+
+// APP CONSTANTS
 const button = document.getElementById('myButton');
 
 button.onclick = () => {
@@ -5,24 +104,37 @@ button.onclick = () => {
     generateArt();
 }
 
+
+// Logic for creating the art
 function generateArt() {
+
+    // Clear mechanism to clear previous art
     const allDivs = document.querySelectorAll('.random-square');
-    // Loop through and remove each div from the DOM
     allDivs.forEach(div => div.remove());
 
-    for (i = 0; i < 100; i++) {
+    // Logic for creating new art
+    for (i = 0; i < inputShapeAmount.value; i++) {
         let newDiv = document.createElement('div');
 
-        // Percentage Roll (not being used currently)
-        const percentA = Math.floor(Math.random() * 100) + 1;
-        const percentB = Math.floor(Math.random() * 100) + 1;
-        const percentC = Math.floor(Math.random() * 100) + 1;
+        // // Percentage Roll (not being used currently)
+        // const percentA = Math.floor(Math.random() * 100) + 1;
+        // const percentB = Math.floor(Math.random() * 100) + 1;
+        // const percentC = Math.floor(Math.random() * 100) + 1;
 
         // RGB Roll
-        const R = Math.floor(Math.random() * 255) + 1;
-        const G = Math.floor(Math.random() * 255) + 1;
-        const B = Math.floor(Math.random() * 255) + 1;
-        const A = Math.floor(Math.random() * 255) + 1;
+        const R = Math.floor(Math.random() * inputRed.value) + 1;
+        const G = Math.floor(Math.random() * inputGreen.value) + 1;
+        const B = Math.floor(Math.random() * inputBlue.value) + 1;
+        const A = Math.floor(Math.random() * inputAlpha.value) + 1;
+
+        // /!\ Implement two aditional random rolls for rgba sequences, for gradient rolls
+        // const rgba2 = {}
+        // const rgba3 = {}
+        
+        // for (let count = 0; count < 3; count ++) {
+
+        // }
+
 
         // Rotate Roll
         const rotateX = Math.floor(Math.random() * 360) + 1;
@@ -31,17 +143,13 @@ function generateArt() {
         const rotateK = Math.floor(Math.random() * 360) + 1;
 
         // Width Height Roll
-
-        // 500x500 Squarish zoomed
-        // 10x500 Wheat Fields
-        // 500x10 
-        const width = Math.floor(Math.random() * 300) + 1; //500 = cool
-        const height = Math.floor(Math.random() * 300) + 1; //500 = cool
+        const width = Math.floor(Math.random() * inputMaxWidth.value) + 1; //500 = cool
+        const height = Math.floor(Math.random() * inputMaxHeight.value) + 1; //500 = cool
 
         // Coordinates X Y Z
-        const X = Math.floor(Math.random() * 100) + 1;
-        const Y = Math.floor(Math.random() * 100) + 1;
-        const Z = Math.floor(Math.random() * 100) + 1;
+        const X = Math.floor(Math.random() * inputX.value) + 1;
+        const Y = Math.floor(Math.random() * inputY.value) + 1;
+        const Z = Math.floor(Math.random() * inputZ.value) + 1;
 
         // CoinFlip: Gradient vs Single Color
         const coinFlip = Math.floor(Math.random() * 2) + 1 === 2;
@@ -81,8 +189,5 @@ function generateArt() {
 }
 }
 
-// const rRange = document.querySelector('r-value').addEventListener('') 
-
-
-// START
+// START (Triggers on Page Reload)
 generateArt();
